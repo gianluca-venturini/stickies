d75da8920aa85826234f9d51c63316ea2b8d5568
Gianluca Venturini <gianluca.91@gmail.com>
2018-10-30 23:04:59 -0700

    Drag & Drop for moving stickies around

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,72 @@ import './App.css';
 
 import { Stickie } from './Stickie';
 
-class App extends React.Component {
+interface IStickie {
+  coordinates: {
+    x: number,
+    y: number,
+  };
+  text: string;
+}
 
+interface IState {
+  stickies: {[id: string]: IStickie}
+}
+
+class App extends React.Component<{}, IState> {
+
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      stickies: {
+        id1: {
+          coordinates: {
+            x: 0,
+            y: 0,
+          },
+          text: 'test123',
+        },
+        id2: {
+          coordinates: {
+            x: 100,
+            y: 100,
+          },
+          text: 'test123',
+        },
+      }
+    }
+  }
 
   public render() {
     return (
       <div className="App">
-        <Stickie />
+        {
+          Object.keys(this.state.stickies).map(stickieId => {
+            const stickie = this.state.stickies[stickieId];
+            return (
+              <Stickie 
+                coordinates={stickie.coordinates} 
+                text={stickie.text} 
+                key={stickieId} 
+                onChangeCoordinates={this.handleChangeCoordinates.bind(this, stickieId)} 
+              />
+            );
+          })
+        }
       </div>
     );
+  }
+
+  private handleChangeCoordinates = (stickieId: string, coordinates: {x: number, y: number}) => {
+    this.setState(s => ({
+      stickies: {
+        ...s.stickies,
+        [stickieId]: {
+          ...s.stickies[stickieId],
+          coordinates
+        }
+      }
+    }));
   }
 }
 
--- src/Stickie.tsx
+++ src/Stickie.tsx
@@ -2,14 +2,62 @@ import * as React from 'react';
 
 import './Stickie.css';
 
-export class Stickie extends React.Component {
+interface ICoordinates {
+  x: number,
+  y: number,
+}
+
+interface IProps {
+  coordinates: ICoordinates;
+  text: string;
+  onChangeCoordinates: (coordinates: ICoordinates) => void;
+}
+
+export class Stickie extends React.Component<IProps, {}> {
+  private mouseDownCoordinates?: ICoordinates;
+  private initialCoordinates?: ICoordinates;
+
   public render() {
+    const {x, y} = this.props.coordinates;
+    const {text} = this.props;
     const style = {
-      left: 0,
-      top: 0,
+      left: x,
+      top: y,
     };
+
     return (
-      <div className="Stickie" contentEditable={true} style={style}/>
+      <div 
+        className="Stickie" 
+        contentEditable={true} 
+        style={style}
+        onMouseDown={this.handleOnMouseDown}
+        onMouseMove={this.handleOnMouseMove}
+        onMouseUp={this.handleOnMouseUp}
+        dangerouslySetInnerHTML={{__html: text}}
+      />
     );
+  }
+
+  private handleOnMouseDown = (e: React.MouseEvent<HTMLElement>) => {
+    this.mouseDownCoordinates = {x: e.clientX, y: e.clientY};
+    this.initialCoordinates = this.props.coordinates;
+  }
+
+  private handleOnMouseUp = (e: React.MouseEvent<HTMLElement>) => {
+    this.mouseDownCoordinates = undefined;
+    this.initialCoordinates = undefined;
+  }
+  
+  private handleOnMouseMove = (e: React.MouseEvent<HTMLElement>) => {
+    const coordinates = {
+      x: e.clientX,
+      y: e.clientY,
+    }
+    if (this.initialCoordinates && this.mouseDownCoordinates) {
+      this.props.onChangeCoordinates({
+        x: this.initialCoordinates.x + (coordinates.x - this.mouseDownCoordinates.x),
+        y: this.initialCoordinates.y + (coordinates.y - this.mouseDownCoordinates.y),
+      });
+    }
   }
 }
